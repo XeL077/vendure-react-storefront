f9284bd983975fbc196de38814ede5ba3e3ed4e4
Alexander Panin <ale21111681@yandex.ru>
2021-05-25 20:26:01 +0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,16 +3,11 @@ import "./Footer.css";
 export function Footer () {
     return (
         <div className="footer">
-            <nav>
-                <a href="/about">О нас</a>
-                <a href="/help">Помощь</a>
-                <a href="/contact">Контакты</a>
-            </nav>
-
-            <span>Социальные сети</span>
-            <nav>
-                <a href="https://www.instagram.com/panny.anny/">instagram</a>
-                <a href="https://vk.com/">vk</a>
+            <nav className="footer-wrapper">
+                <a className="footer-link" href="/about">О нас</a>
+                <a className="footer-link" href="/contact">Контакты</a>
+                <a className="footer-link" href="/help">Помощь</a>
+                <a className="footer-link" href="/payments">Оплата и доставка</a>
             </nav>
         </div>
     );
